893209277709818263bb7c68f4ee238978274fcb
CaroleSa <carole220287@hotmail.com>
2019-09-18 16:11:24 +0200

    test call_api in progress 0

--- program/tests/test_call_api.py
+++ program/tests/test_call_api.py
@@ -3,3 +3,36 @@
 
 
 
+from io import BytesIO
+import json
+import requests
+
+
+from program.classes import call_api as ca
+
+
+
+# custom class to be the mock return value
+# will override the requests.Response returned from requests.get
+class MockResponse:
+
+    # mock json() method always returns a specific testing dictionary
+    @staticmethod
+    def json():
+        return {"mock_key": "mock_response"}
+
+
+def test_get_place_data(monkeypatch):
+
+    # Any arguments may be passed and mock_get() will always return our
+    # mocked object, which only has the .json() method.
+    def mockreturn(*args, **kwargs):
+        return {"mock_key": "mock_response"}
+
+    # apply the monkeypatch for requests.get to mock_get
+    monkeypatch.setattr(requests, "get", mockreturn)
+
+    # app.get_json, which contains requests.get, uses the monkeypatch
+    new_callapimaps = ca.CallApiMaps()
+    result = new_callapimaps.get_place_data("toulouse")
+    assert result["mock_key"] == "mock_response"
