a1e48eddc0e3e28ba3172f5b26759308021fd95e
CaroleSa <carole220287@hotmail.com>
2019-09-18 14:51:15 +0200

    test parser ok

--- program/tests/test_parser.py
+++ program/tests/test_parser.py
@@ -3,18 +3,18 @@
 
 from program.classes import parser as pa
 
+class Test:
+    def setup_method(self):
+        new_parser = pa.Parser()
+        self.results_lower = new_parser.get_place_searched("Openclassrooms")
+        self.results_no_symbols = new_parser.get_place_searched("saint-nazaire")
+        self.results_no_common_words = new_parser.get_place_searched("je cherche la mairie de caluire")
 
-def test_get_place_searched():
-    new_parser = pa.Parser()
-    results = new_parser.get_place_searched("Openclassrooms")
-    assert results == "openclassrooms"
+    def test_lower(self):
+        assert self.results_lower == "openclassrooms"
 
-"""def test_get_place_searched():
-    new_parser = pa.Parser()
-    results = new_parser.get_place_searched("saint-nazaire")
-    assert results == "saint nazaire"
+    def test_no_symbols(self):
+        assert self.results_no_symbols == "saint nazaire"
 
-def test_get_place_searched():
-    new_parser = pa.Parser()
-    results = new_parser.get_place_searched("je cherche la mairie de caluire")
-    assert results == "mairie caluire"""""
+    def test_no_common_words(self):
+        assert self.results_no_common_words == "mairie caluire"
