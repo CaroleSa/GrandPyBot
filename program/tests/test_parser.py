e4f00fbf27a3c05e5885bc5122d1a771f1f4992c
CaroleSa <carole220287@hotmail.com>
2019-09-19 14:06:12 +0200

    add test in test_parser ok

--- program/tests/test_parser.py
+++ program/tests/test_parser.py
@@ -7,14 +7,26 @@ class Test:
     def setup_method(self):
         self.new_parser = pa.Parser()
 
+    def test_empty(self):
+        results_empty = self.new_parser.get_place_searched("")
+        assert results_empty == ""
+
+    def test_space(self):
+        results_space = self.new_parser.get_place_searched("    ")
+        assert results_space == ""
+
     def test_lower(self):
-        self.results_lower = self.new_parser.get_place_searched("Openclassrooms")
-        assert self.results_lower == "openclassrooms"
+        results_lower = self.new_parser.get_place_searched("Openclassrooms")
+        assert results_lower == "openclassrooms"
 
     def test_no_symbols(self):
-        self.results_no_symbols = self.new_parser.get_place_searched("saint-nazaire")
-        assert self.results_no_symbols == "saint nazaire"
+        results_no_symbols = self.new_parser.get_place_searched("saint-nazaire")
+        assert results_no_symbols == "saint nazaire"
 
-    def test_no_common_words(self):
-        self.results_no_common_words = self.new_parser.get_place_searched("je cherche la mairie de caluire")
-        assert self.results_no_common_words == "mairie caluire"
+    def test_get_place(self):
+        results_no_common_words = self.new_parser.get_place_searched("je cherche la mairie de caluire")
+        assert results_no_common_words == "mairie caluire"
+
+    def test_stop_words(self):
+        results_stop_words = self.new_parser.get_place_searched("je cherche un lieu")
+        assert results_stop_words == ""
