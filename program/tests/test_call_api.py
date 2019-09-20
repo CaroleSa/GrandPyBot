e790be020407519e4cd6f01811106a358c8109bc
CaroleSa <carole220287@hotmail.com>
2019-09-20 15:08:51 +0200

    call_api test in progress

--- program/tests/test_call_api.py
+++ program/tests/test_call_api.py
@@ -11,50 +11,29 @@ import requests
 from program.classes import call_api as ca
 
 
-
-# custom class to be the mock return value
-# will override the requests.Response returned from requests.get
 class MockResponse:
+    '''Mock for requests.get response call'''
+    def __init__(self, result, ok=True):
+        self.ok = ok
+        self.result = result
 
-    # mock json() method always returns a specific testing dictionary
-    @staticmethod
-    def json():
-        return {"mock_key": "mock_response"}
+    def json(self):
+        return self.result
 
+class TestGoogleApi:
+    '''Testing the google api methods calls'''
 
-def test_get_place_data(monkeypatch):
+    def setup_method(self):
+        self.place = "lyon"
 
-    # Any arguments may be passed and mock_get() will always return our
-    # mocked object, which only has the .json() method.
-    def mockreturn(*args, **kwargs):
-        return {"mock_key": "mock_response"}
+    def test_search_ok(self, monkeypatch):
+        def mockreturn(url, params):
+            return MockResponse({
+            'candidates': [{'formatted_address': 'Lyon, France',
+                            'geometry': {'location': {'lat': 45.764043, 'lng': 4.835659},
+                                         'viewport': {'northeast': {'lat': 45.808425, 'lng': 4.898393},
+                                                      'southwest': {'lat': 45.707486, 'lng': 4.7718489}}}}], 'status': 'OK'})
+        monkeypatch.setattr(requests, "get", mockreturn)
+        new_callapimaps = ca.CallApiMaps()
+        assert new_callapimaps.get_place_data(self.place) is not None
 
-    # apply the monkeypatch for requests.get to mock_get
-    monkeypatch.setattr(requests, "get", mockreturn)
-
-    # app.get_json, which contains requests.get, uses the monkeypatch
-    new_callapimaps = ca.CallApiMaps()
-    result = new_callapimaps.get_place_data("toulouse")
-    assert result["mock_key"] == "mock_response"
-
-
-
-
-    self.mock = {'product_name_fr': 'product_name',
-                 'nutriments': {
-                     'nutrition-score-fr': 25},
-                 'image_front_url': 'image_url.com',
-                 'url': 'url.com'}
-
-    @patch('requests.get')
-    def test_imports(self, mock_api):
-        """Testing if the method only keep result with every wanted elements"""
-        self.response["products"].extend([self.mock, self.mock_no_url,
-                                          self.mock_no_img, self.mock_no_score,
-                                          self.mock_no_name])
-        mock_api.return_value.json.return_value = self.response
-        result = ('product_name',
-                  25,
-                  'image_url.com',
-                  'url.com')
-        self.assertEqual(self.api.get_products('test'), [result])
