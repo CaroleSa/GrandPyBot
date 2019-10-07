dd74575023c3404cf2d7bdc48e0c5dcf0742795a
CaroleSa <carole220287@hotmail.com>
2019-10-07 14:18:41 +0200

    js, css html ok 0

--- program/static/script.js
+++ program/static/script.js
@@ -1,39 +1,62 @@
+// hide the loader and the map
 $('#loader').hide();
 $('#space2').show();
 $("#rowMap").hide();
 
-
+// getQuestionInThread function
 function getQuestionInThread() {
+    // get user's question
     var $textareaValueElt = $('textarea').val();
+
+    // display user's question in the thread
     $('#messageThread').append("<br><p id='message'><span id='userName'> Utilisateur : <br></span>" + $textareaValueElt + "</p>");
+    // delete TextMap in the last GrandPy Bot's answer
     $(".textMap").remove();
+
+    // display the last text in the thread
     element = document.getElementById('thread');
 	element.scrollTop = element.scrollHeight;
+
+	// use ajax
     $.ajax({
+            // post user's question in the backend
 			data : {question: $('textarea').val()},
 			type : 'POST',
 			url : '/process'
 	})
 
 	.done(function(data) {
+	    // hide loader
 	    $('#loader').hide();
 	    $('#space2').show();
 
 	    if(data.error){
+	            // display GrandPy Bot's answer in the thread
 				$('#messageThread').append("<br><p id='message'><span id='robotName'> GrandPy Bot : <br></span>" + data.error + "</p>");
+
+				// display the last text in the thread
 				element = document.getElementById('thread');
 	            element.scrollTop = element.scrollHeight;
 				return
 
 		}if(data.latitude){
+		        // display GrandPy Bot's answer in the thread
                 $('#messageThread').append("<br><p id='message'><span id='robotName'> GrandPy Bot : <br></span>" + data.address + "</p>");
+
+                // display the last text in the thread
                 element = document.getElementById('thread');
 	            element.scrollTop = element.scrollHeight;
+
+	            // display GrandPy Bot's answer in the thread
 	            $('#messageThread').append("<p id='message'>" + data.history + "</p>");
 	            $('#messageThread').append("<p class='textMap' id='message'>" + data.map + "</p>");
+
+	            // display the map
                 $("#rowMap").show();
                 var map;
                 var marker;
+
+                // initMap function: creation of the map
                 function initMap() {
                     var LatLng = {lat: data.latitude, lng: data.longitude};
                     map = new google.maps.Map(document.getElementById('map'), {
@@ -42,48 +65,35 @@ function getQuestionInThread() {
                     });
                     marker = new google.maps.Marker({
                     position: LatLng,
-                    map: map,
-                    title: 'Hello World!'
+                    map: map
                     });
                 }
-
+                // call initMap function
                 initMap();
-                $('#map').css('height','180px');
-                $('#map').css('max-width','100%');
-                $('#map').css('margin','10px');
-                $('#map').css('border-left','5px white inset');
-                $('#map').css('border-right',' 5px white inset');
-                $('#map').css('border-top',' 5px white inset');
-                $('#map').css('border-bottom',' 5px white inset');
-
-                }else{
-                    $('#messageThread').append("<br><p id='message'><span id='robotName'> GrandPy Bot : <br></span>" + data.address + "</p>");
-                    element = document.getElementById('thread');
-	                element.scrollTop = element.scrollHeight;
-	                $('#messageThread').append("<p id='message'>" + data.history + "<br>" + data.map + "</p>");
-                    }
+                }
     });
+    // delete user's question in textarea
  	$('textarea').val('').change();
 }
 
-
+// if the user click in the button
 $("button").on('click', function () {
+    // display loader and hide the map
     $('#loader').show();
     $('#space2').hide();
     $("#rowMap").hide();
+    // call getQuestionInThread function
     getQuestionInThread();
 });
 
-
+// if the user presses the space key
 $("textarea").keyup(function(e) {
     if (e.keyCode == 13) {
+        // display loader and hide the map
         $('#loader').show();
         $('#space2').hide();
         $("#rowMap").hide();
+        // call getQuestionInThread function
         getQuestionInThread();
     }
 });
-
-
-
-
